a505cb82ae29138b32c07b3c6b9f79ef4ea40d8c
Billy K. Poon <bkpoon@lbl.gov>
2025-08-22 08:57:32 -0700

    Reorganize run_tests.py so that libtbx.run_tests_parallel works

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,12 @@
 from libtbx import test_utils
 import libtbx.load_env
 
-def run():
-  tst_list = (
+tst_list = (
   "$D/tst_tntbx_ext.py",
   "$D/tst_large_eigen.py",
-  )
+)
+
+def run():
 
   build_dir = libtbx.env.under_build("tntbx")
   dist_dir = libtbx.env.dist_path("tntbx")
